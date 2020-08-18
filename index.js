96f023e78beda8d22eaf5fa514019cd6e368a94d
chentong <chentong@iqiguo.com>
2020-08-18 15:51:43 +0800

    fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.all('*',function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization,Origin,Accept,X-Requested-With');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('X-Powered-By', '陈通');
+    res.header('X-Powered-By', 'chentong');
     res.header('Content-Type', 'application/json;charset=utf-8');
     if (req.method == 'OPTIONS') {
         res.sendStatus(200);
